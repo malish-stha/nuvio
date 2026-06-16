fe6633f199171aca1419ec611c049e79f5293469
malish-stha <malish.shrestha@deerwalk.edu.np>
2026-06-16 09:01:33 +0545

    chore: disable static export output when deploying to Vercel in next.config.ts

--- renderer/next.config.ts
+++ renderer/next.config.ts
@@ -1,8 +1,10 @@
 import { NextConfig } from 'next'
 
+const isVercel = process.env.VERCEL === '1' || !!process.env.VERCEL
+
 const config: NextConfig = {
-  output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
-  distDir: process.env.NODE_ENV === 'production' ? '../app' : '.next',
+  output: (process.env.NODE_ENV === 'production' && !isVercel) ? 'export' : undefined,
+  distDir: (process.env.NODE_ENV === 'production' && !isVercel) ? '../app' : '.next',
   trailingSlash: true,
   images: {
     unoptimized: true,
